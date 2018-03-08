f5f11e56d1efc0422f4566213829edf9ec3cde8b
Aparna Jha <aparnajha482@gmail.com>
2018-03-08 13:16:49 +0530

    entered token received from twilio

--- Project_Message_Sender.py
+++ Project_Message_Sender.py
@@ -2,3 +2,5 @@ from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
 account_sid = "AC5e2fe565c5c374f82fde0e5b7b3375ea"
+# Your Auth Token from twilio.com/console
+auth_token  = "cf659cc08aea1858c713a135ff585ced"

e6f06919daf3a8655da06fec2bd95808e98c2bea
Aparna Jha <aparnajha482@gmail.com>
2018-03-08 20:19:55 +0530

    hide API KEY for privacy.

--- Project_Message_Sender.py
+++ Project_Message_Sender.py
@@ -3,7 +3,7 @@ from twilio.rest import Client
 # Your Account SID from twilio.com/console
 account_sid = "AC5e2fe565c5c374f82fde0e5b7b3375ea"
 # Your Auth Token from twilio.com/console
-auth_token  = "cf659cc08aea1858c713a135ff585ced"
+auth_token  = "enter your TOKEN of twilio"
 
 client = Client(account_sid, auth_token)
 
